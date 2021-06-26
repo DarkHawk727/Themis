843cde046d333cf3955810d2c024886219424bce
yum <antriksh.timepass@gmail.com>
2021-06-27 03:55:12 +0530

    complete todays headline

--- server/app.py
+++ server/app.py
@@ -44,6 +44,31 @@ def do():
 @app.route('/today', methods=["GET"])
 def todaysHeadlines():
    res = requests.get(f'https://newsdata.io/api/1/news?apikey=pub_42213970ad6b67a6efb40d24a9038c93875&category=health,business&language=en') 
-   return res.json()
+   summaries = []
+   results = res.json()["results"]
+
+   for result in results:
+       text = ""
+       if not result["content"]:
+           text += result["description"]
+       else:
+           text += result["content"]
+
+       summary = summarizer(result["description"], max_length=200, min_length=90, do_sample=False)
+       blob = TextBlob(summary[0]["summary_text"])
+       summaries.append({
+           "headline": result['title'] or '',
+           "image": result['image_url'] or '',
+           "source": result['source_id'] or '',
+           "summary": summary[0]["summary_text"],
+           "polarity": blob.sentiment[0],
+           "subjectivity": blob.sentiment[1],
+           "reading_level": textstat.flesch_reading_ease(summary[0]["summary_text"]) 
+       })
+       
+   return {
+       "summaries": summaries
+   }
+
 if __name__ == '__main__':
     app.run()
