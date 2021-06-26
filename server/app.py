b8a065cece436272a726d04e00c33b99a8550f44
yummyweb <antriksh.timepass@gmail.com>
2021-06-26 20:56:15 +0530

    adding gnews

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from transformers import pipeline
 from textblob import TextBlob
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +10,8 @@ summarizer = pipeline("summarization")
 @app.route('/', methods=['POST'])
 def do():
     body = request.get_json()
+    res = requests.get(f'https://gnews.io/api/v4/search?q={body["query"]}&token=9f533fdd5b97ecdf7aee2a444643c5ef')
+    print(res.content())
     summary = summarizer(body["text"], max_length=200, min_length=90, do_sample=False)
     blob = TextBlob(summary[0]["summary_text"])
     data = {
