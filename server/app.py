5942525bcc76f5f9637767e7357eca4c6361d42a
yummyweb <antriksh.timepass@gmail.com>
2021-06-26 20:21:15 +0530

    the text is now summarised and polarity and subjectiivity metrics in place too

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from transformers import pipeline
+from textblob import TextBlob
 
 app = Flask(__name__)
 
@@ -8,10 +9,13 @@ summarizer = pipeline("summarization")
 @app.route('/', methods=['POST'])
 def do():
     body = request.get_json()
-    summary = summarizer(body["text"], max_length=100, min_length=90, do_sample=False)
+    summary = summarizer(body["text"], max_length=200, min_length=90, do_sample=False)
+    blob = TextBlob(summary[0]["summary_text"])
     data = {
         "data": {
-            "summary": summary[0]["summary_text"]
+            "summary": summary[0]["summary_text"],
+            "polarity": blob.sentiment[0],
+            "subjectivity": blob.sentiment[1],
         }
     }
     return data
