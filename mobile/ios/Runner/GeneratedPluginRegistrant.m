81bc86f7a6ff61ac24a5951121d92b698a06f621
Connor Wilson <cnwilson29@gmail.com>
2021-06-26 20:22:58 -0400

    pog shit

--- mobile/ios/Runner/GeneratedPluginRegistrant.m
+++ mobile/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<url_launcher/FLTURLLauncherPlugin.h>)
+#import <url_launcher/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
 }
 
 @end
